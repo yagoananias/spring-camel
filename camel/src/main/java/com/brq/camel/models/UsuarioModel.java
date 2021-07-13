09af1bef94b7c7a053e4165cd28d8df7aa69519e
Yago Ananias <yago_souza92@yahoo.com.br>
2021-07-13 17:25:15 -0300

    Subindo o projeto :tada:

---
+++ camel/src/main/java/com/brq/camel/models/UsuarioModel.java
@@ -0,0 +1,39 @@
+package com.brq.camel.models;
+
+public class UsuarioModel {
+	private int id;
+	private String name;
+	private String email;	
+	
+	public UsuarioModel(int id, String name, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+	
+	public UsuarioModel() {}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+
+}
